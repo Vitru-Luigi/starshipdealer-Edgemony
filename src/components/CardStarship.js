26df92ca1155e2b67a6fee16fa3f11213f96fda5
vitru <vitru.luigi@gmail.com>
2021-03-22 17:59:40 +0100

    minor

--- src/components/CardStarship.js
+++ src/components/CardStarship.js
@@ -2,29 +2,29 @@ import React from 'react';
 import {Col, Card, Container, ListGroup} from 'react-bootstrap';
 
 const CardStarship = ({starships}) => {
-	return starships.map((startship) => (
-		<Col md={6} lg={12} key={startship.id}>
+	return starships.map((starship) => (
+		<Col md={6} lg={4} key={starship.id}>
 			<Card>
-				<Card.Header as='h5'>{startship.name}</Card.Header>
-				<Card.Img src={startship.image} />
+				<Card.Header as='h5'>{starship.name}</Card.Header>
+				<Card.Img src={starship.image} alt={starship.name} />
 				<Card.Body>
-					<Card.Title>{startship.model}</Card.Title>
-					<Card.Text>{startship.manufacturer}</Card.Text>
+					<Card.Title>{starship.model}</Card.Title>
+					<Card.Text>{starship.manufacturer}</Card.Text>
 				</Card.Body>
 				<Container>
 					<ListGroup variant='flush'>
-						<ListGroup.Item>length: {startship.length} meters</ListGroup.Item>
-						<ListGroup.Item>crew: {startship.crew}</ListGroup.Item>
-						<ListGroup.Item>passengers: {startship.passengers}</ListGroup.Item>
-						<ListGroup.Item>cargo capacity: {startship.cargo_capacity} meters</ListGroup.Item>
-						<ListGroup.Item>consumables: {startship.consumables} </ListGroup.Item>
-						<ListGroup.Item>hyperdrive rating: {startship.hyperdrive_rating} </ListGroup.Item>
-						<ListGroup.Item>max atmosphering speed: {startship.max_atmosphering_speed} </ListGroup.Item>
-						<ListGroup.Item>MGLT: {startship.MGLT} </ListGroup.Item>
+						<ListGroup.Item>length: {starship.length} meters</ListGroup.Item>
+						<ListGroup.Item>crew: {starship.crew}</ListGroup.Item>
+						<ListGroup.Item>passengers: {starship.passengers}</ListGroup.Item>
+						<ListGroup.Item>cargo capacity: {starship.cargo_capacity} meters</ListGroup.Item>
+						<ListGroup.Item>consumables: {starship.consumables} </ListGroup.Item>
+						<ListGroup.Item>hyperdrive rating: {starship.hyperdrive_rating} </ListGroup.Item>
+						<ListGroup.Item>max atmosphering speed: {starship.max_atmosphering_speed} </ListGroup.Item>
+						<ListGroup.Item>MGLT: {starship.MGLT} </ListGroup.Item>
 					</ListGroup>
 				</Container>
 				<Card.Footer>
-					<small className='text-muted'>Credits : {startship.cost_in_credits}</small>
+					<small className='text-muted'>Credits : {starship.cost_in_credits}</small>
 				</Card.Footer>
 			</Card>
 		</Col>
